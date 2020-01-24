ff4ae9bfa4b7a13e04ee1170e8a25844f21efdcb
Nany Bennett <nani.steele@hotmail.com>
2020-01-23 18:49:31 -0600

    Conexion y primer tabla

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+class Conexion{
+    private $usuario="root";
+    private $base="erp";
+    private $password="";
+    private $host="localhost";
+    protected $sentencia= "";
+    public $conexion;
+    private function abrirConexion(){
+        $this->conexion =new mysqli($this->host,$this->usuario,$this->password,$this->base);
+    }
+    private function cerrarConexion(){
+        $this->conexion->close();
+    }//Alta,Baja,Modificaciones.
+    public function ejecutarSentencia(){
+        $this->abrirConexion();
+        $this->conexion->query($this->sentencia);
+        $this->cerrarConexion();
+    }
+    public function obtenerSentencia(){
+        $this->abrirConexion();
+        $resultado= $this->conexion->query($this->sentencia);
+        return $resultado;
+    }
+}
+?>
